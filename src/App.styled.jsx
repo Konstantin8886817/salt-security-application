da14ac3d1e54f564ace7a7e7d6d0379abd540bb4
Konstantin / Front-End Developer <61967294+Konstantin8886817@users.noreply.github.com>
2022-07-08 23:00:33 +0300

    installed dependencies

---
+++ src/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const LandingPageContainer = styled.div`
+height: 100vh;
+`;
+
+export const Content = styled.div`
+padding: 30px;
+background-color: #F0F0F0;
+height: -webkit-fill-available;
+`;
